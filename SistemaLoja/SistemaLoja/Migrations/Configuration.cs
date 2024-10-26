f12b1c151b7d9e5312f106da64aaa5c91be1bde0
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-10-26 16:20:02 -0300

    Update 2024-10-26   16h19
    Migration automática
    Update 2024-10-26   16h19

---
+++ SistemaLoja/SistemaLoja/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace SistemaLoja.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SistemaLoja.Data.SistemaLojaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "SistemaLoja.Data.SistemaLojaContext";
+        }
+
+        protected override void Seed(SistemaLoja.Data.SistemaLojaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
